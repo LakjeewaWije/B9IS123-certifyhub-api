148a6cf5c27a0114ec83906826907a9935691e20
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-14 15:08:34 +0000

    Initialized : firebae db

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const admin = require('firebase-admin');
 const app = express()
 const port = 3000
 
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://b9is123-certifyhub-default-rtdb.europe-west1.firebasedatabase.app/'
+});
+
+const db = admin.database();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!')
+  res.send('Hello World!!!')
+})
+
+app.get('/add', (req, res) => {
+  db.ref('/user/sfasdfsf').set({
+    key1: 'value1',
+    key2: 'value2'
+  });
+  res.send('added')
 })
 
 app.listen(port, () => {
